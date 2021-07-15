9df38d56f06a52961faebf2770a0bd834248aa03
AiaRup <aiale88@gmail.com>
2021-07-15 19:36:42 +0300

    add some translations for thai language

--- src/i18n/languages/en.js
+++ src/i18n/languages/en.js
@@ -1,12 +1,29 @@
 const translations = {
-  ok: 'OK',
-  confirm: 'Confirm',
+  done: 'Done',
   cancel: 'Cancel',
-  finish: 'Finish',
-  Language: 'Language',
-  confirmLogout: 'Are you sure you want to log out?',
-  userInfo: 'User info',
-  disconnect: 'Disconnect'
+  none: 'None',
+  or: 'Or',
+  close: 'Close',
+  yes: 'Yes',
+  back: 'Back',
+  next: 'Next',
+  error: 'Error',
+  all: 'All',
+  logout: 'Log Out',
+
+  // login form
+  password: 'Password',
+  username: 'Username',
+  login: 'Login',
+  forgotPassword: 'Forgot password',
+  rememberMe: 'Remember me',
+  registerNow: 'Register now',
+
+  // register form
+  email: 'Email',
+  confirmPassword: 'Confirm Password',
+  register: 'Register',
+  alreadyRegistered: 'Already Registered?'
 };
 
 export default translations;
--- src/i18n/languages/th.js
+++ src/i18n/languages/th.js
@@ -1,12 +1,29 @@
 const translations = {
-  ok: 'OK',
-  confirm: 'Confirm',
-  cancel: 'Cancel',
-  finish: 'Finish',
-  Language: 'Language',
-  confirmLogout: 'Are you sure you want to log out?',
-  userInfo: 'User info',
-  disconnect: 'Disconnect'
+  done: 'เสร็จแล้ว',
+  cancel: 'ยกเลิก',
+  none: 'ไม่มี',
+  or: 'หรือ',
+  close: 'ปิด',
+  yes: 'ใช่',
+  back: 'ย้อนกลับ',
+  next: 'ต่อไป',
+  error: 'ผิดพลาด',
+  all: 'ทั้งหมด',
+  logout: 'ออกจากระบบ',
+
+  // login form
+  password: 'รหัสผ่าน',
+  username: 'ชื่อผู้ใช้',
+  login: 'เข้าสู่ระบบ',
+  forgotPassword: 'ลืมรหัสผ่าน',
+  rememberMe: 'จดจำฉัน',
+  registerNow: 'สมัครตอนนี้',
+
+  // register form
+  email: 'อีเมล',
+  confirmPassword: 'ยืนยันรหัสผ่าน',
+  register: 'การลงทะเบียน',
+  alreadyRegistered: 'ลงทะเบียนเรียบร้อยแล้ว?'
 };
 
 export default translations;
